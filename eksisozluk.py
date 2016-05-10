a83c6d67c9710127c0922d63e2a63186c0625e96
Coşkun Deniz <coskun.denize@gmail.com>
2016-05-10 18:01:11 +0300

    added script

---
+++ eksisozluk.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from collections import defaultdict
+from argparse import ArgumentParser
+from urllib2 import urlopen
+from bs4 import BeautifulSoup
+
+
+PAGE_URL = 'https://eksisozluk.com'
+
+
+def parse_page(page_url):
+    """Open and parse page content
+
+    :rtype: object
+    :returns: parsed page as a BeautifulSoup object
+    """
+
+    page = urlopen(page_url)
+    parsed_page = BeautifulSoup(page, 'lxml')
+
+    return parsed_page
+
+
+def get_topics():
+    """Get list of popular topics
+
+    :rtype: list
+    :returns: list of extracted results that contains a tuple for each topic
+    """
+
+    results = []
+
+    parsed_page = parse_page(PAGE_URL)
+
+    topic_list = parsed_page.find('ul', 'topic-list partial')
+
+    links = topic_list('a')
+
+    for index, link in enumerate(links):
+        topic_link = link['href'].encode('utf-8')
+        topic_name = link.contents[0].encode('utf-8')
+        number_of_entries_for_topic = int(link.contents[1].string)
+        results.append((index+1, topic_link, topic_name, number_of_entries_for_topic))
+
+    return results
+
+
+def get_entries_for_topic(topic_link, entry_count_per_topic):
+    """Get last entries for a topic. Number of entries is ten by default, or
+       less than ten as requested count per topic.
+
+    :type topic_link: string
+    :param topic_link: link of topic
+    :type entry_count_per_topic: integer
+    :param entry_count_per_topic: number of entries requested per topic
+    :rtype: list
+    :returns: list of entries for a topic
+    """
+
+    # open and parse page for given topic_link
+    topic_page_content = parse_page(PAGE_URL + topic_link)
+    entry_list_container = topic_page_content.find('ul', {'id': 'entry-list'})
+    entries_for_topic = entry_list_container.findAll('div', 'content')[:entry_count_per_topic]
+
+    return [entry.text for entry in entries_for_topic]
+
+
+def get_topics_sorted_by_entry_count(results):
+    """Get first ten topics according to entry count
+
+    :type results: list
+    :param results: list of topic results
+    :rtype: list
+    :returns: list of topics sorted by entry count
+    """
+
+    return sorted(results, key=lambda x: x[3], reverse=True)[:10]
+
+
+def get_selected_topic_indexes():
+    """Get index of selected topics from user
+
+    :rtype: list
+    :returns: list of indexes
+    """
+
+    topic_indexes = raw_input("\nEnter requested topic indexes: ")
+
+    if topic_indexes == "e":
+        sys.exit(1)
+
+    selected_topic_indexes = [int(index) for index in list(topic_indexes.split())
+                                         if index.isdigit()]
+
+    return selected_topic_indexes
+
+
+def get_entries_for_selected_topics(results, selected_topic_indexes, entry_count_per_topic):
+    """Get entries for selected topics
+
+    :type results: list
+    :param results: list of topic results
+    :type selected_topic_indexes: list
+    :param selected_topic_indexes: list of selected indexes
+    :type entry_count_per_topic: integer
+    :param entry_count_per_topic: number of entries requested per topic (default=10)
+    :rtype: dictionary
+    :returns: entries for selected topics as name: entry_list pairs
+    """
+
+    selected_topic_links = []
+    selected_topic_entries = defaultdict(list)
+
+    for topic_index in selected_topic_indexes:
+        for topic_result in results:
+            if topic_index == topic_result[0]:
+                topic_link = topic_result[1]
+                topic_name = topic_result[2]
+
+                selected_topic_links.append(topic_link)
+                selected_topic_entries[topic_name] = get_entries_for_topic(topic_link, entry_count_per_topic)
+
+    return selected_topic_entries
+
+
+def list_topics(print_top_ten=False):
+    """Print currently popular topics
+
+    :type print_top_ten: bool
+    :param print_top_ten: If True, print top ten entries according to entry count
+    """
+
+    topic_results = get_topics()
+
+    if print_top_ten:
+        most_entry_topics = get_topics_sorted_by_entry_count(topic_results)
+
+        print
+        for index, (t_index, t_link, t_name, t_entry_count) in enumerate(most_entry_topics):
+            print index+1, t_name, t_entry_count
+
+    print
+    for (t_index, t_link, t_name, t_entry_count) in topic_results:
+        print t_index, t_name
+
+
+def print_results(results):
+    """Print entries for selected topics
+
+    :type results: dictionary
+    :param results: entries for selected topics as name: entry_list dict
+    """
+
+    for topic_name, topic_entries in results.iteritems():
+
+        print "#" * len(topic_name)
+        print topic_name
+        print "#" * len(topic_name)
+
+        for index, entry in enumerate(topic_entries):
+            print "\n%2s - %s" % (index+1, entry.strip().encode('utf-8'))
+            print "-" * 80
+
+
+if __name__ == '__main__':
+
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("-c", "--count", default=10, type=int, dest="entry_count_per_topic",
+                            help="Number of entries for a topic to extract")
+
+    args = arg_parser.parse_args()
+    topic_results = get_topics()
+
+    list_topics(True)
+
+    requested_topic_indexes = get_selected_topic_indexes()
+    requested_topic_entries = get_entries_for_selected_topics(topic_results,
+                                                              requested_topic_indexes,
+                                                              args.entry_count_per_topic)
+
+    print_results(requested_topic_entries)
